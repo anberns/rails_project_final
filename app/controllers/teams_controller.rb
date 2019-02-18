57b82be0c5590c5d26f0e7733b8a89a8d9bb6a15
Aaron Berns <anberns@gmail.com>
2019-02-18 17:29:02 -0500

    add team 'edit' action and view, limited to coaches

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -12,7 +12,30 @@ class TeamsController < ApplicationController
     end
   end
 
+  def edit
+    @team = Team.find(params[:id])
+    if session[:user_type] != 'coach' || !authorized?(@team)
+      return head(:forbidden)
+    end
+  end
+
+  def update
+    @team = Team.find(params[:id])
+    if session[:user_type] != 'coach' || !authorized?(@team)
+      return head(:forbidden)
+    end
+    @team.update(team_params)
+    redirect_to team_path(@team.id)
+  end 
+
   private
+
+  def team_params
+    params.require(:team).permit(
+      :name,
+      :hq
+    )
+  end
 
   def require_login 
     return head(:forbidden) unless session.include? :user_id 
