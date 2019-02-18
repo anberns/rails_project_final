5cbd35b3c3ca90b4bcecc718904502b77e9751a3
Aaron Berns <anberns@gmail.com>
2019-02-18 14:52:45 -0500

    add session functionality, login, logout buttons

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,28 @@
-class SessionsController < ApplicationRecord
+class SessionsController < ApplicationController
 
   def new
   end
 
   def create
-    session[:user_id] = params[:user_id]
-    redirect_to '/'
+    if params[:user_type] == "coach"
+      coach = Coach.find_by(email: params[:email])
+      return head(:forbidden) unless coach.authenticate(params[:password])
+      session[:user_id] = coach.id 
+      session[:user_type] = "coach"
+      redirect_to coach_path(coach.id)
+    else
+      swimmer = Swimmer.find_by(email: params[:email])
+      return head(:forbidden) unless swimmer.authenticate(params[:password])
+      session[:user_id] = swimmer.id 
+      session[:user_type] = "swimmer"
+      redirect_to swimmers_path(swimmer.id)
+    end
   end
 
   def destroy
-    session.delete :user_id
+    session.delete :user_id 
+    session.delete :user_type
+    redirect_to '/'
   end
   
 end
