57e7036530a015af0c00d153b2a29ab4a218a505
Aaron Berns <anberns@gmail.com>
2019-02-18 10:49:35 -0500

    create sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationRecord
+
+  def new
+  end
+
+  def create
+    session[:user_id] = params[:user_id]
+    redirect_to '/'
+  end
+
+  def destroy
+    session.delete :user_id
+  end
+  
+end
