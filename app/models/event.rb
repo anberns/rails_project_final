58a642bc2cf61787f1dbc4c091ef3d718598ccbd
Aaron Berns <anberns@gmail.com>
2019-02-19 10:01:42 -0500

    add Event.sort method to display events by distance on event index page

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,8 @@ class Event < ApplicationRecord
   validates :name, presence: true, uniqueness: true 
   validates :distance, presence: true
   validates :stroke, presence: true
+
+  def self.sort
+    sorted_events = Event.all.sort_by { |event| event.distance }
+  end
 end
