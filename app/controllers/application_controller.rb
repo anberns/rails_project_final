7aa951b9530f68b8301f38755752df6870d13cd0
Aaron Berns <anberns@gmail.com>
2019-02-19 13:35:01 -0500

    add navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  def current_swimmer
+    @current_swimmer ||= Swimmer.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_swimmer
 end
