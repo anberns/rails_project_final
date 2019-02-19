925c1540e7ee2903fb6ddf141102842b351d7893
Aaron Berns <anberns@gmail.com>
2019-02-19 09:40:18 -0500

    add clause to swimmer.events_attributes to check for blank new event on swimmer signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,27 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if params[:user_type] == "coach"
-      coach = Coach.find_by(email: params[:email])
-      if !coach
-        flash[:notice] = "Account not found."
-        return redirect_to '/'
-      end
-      return head(:forbidden) unless coach.authenticate(params[:password])
-      session[:user_id] = coach.id 
-      session[:user_type] = "coach"
-      redirect_to coach_path(coach.id)
-    else
-      swimmer = Swimmer.find_by(email: params[:email])
-      if !swimmer
-        flash[:notice] = "Account not found."
-        return redirect_to '/'
-      end
-      return head(:forbidden) unless swimmer.authenticate(params[:password])
-      session[:user_id] = swimmer.id 
-      session[:user_type] = "swimmer"
-      redirect_to swimmer_path(swimmer.id)
+    swimmer = Swimmer.find_by(email: params[:email])
+    if !swimmer
+      flash[:notice] = "Account not found."
+      return redirect_to '/'
     end
+    return head(:forbidden) unless swimmer.authenticate(params[:password])
+    session[:user_id] = swimmer.id 
+    session[:user_type] = "swimmer"
+    redirect_to swimmer_path(swimmer.id)
   end
 
   def destroy
--- app/models/swimmer.rb
+++ app/models/swimmer.rb
@@ -7,9 +7,12 @@ class Swimmer < ApplicationRecord
   has_many :events, through: :swimmer_events, dependent: :destroy
   accepts_nested_attributes_for :events
   def events_attributes=(events_attributes)
+    puts events_attributes
     events_attributes.values.each do |event_attribute|
-      event = Event.find_or_create_by(event_attribute)
-      self.events << event
+      if event_attribute[:name] != "" || event_attribute[:distance] != "" || event_attribute[:stroke] != ""
+        event = Event.find_or_create_by(event_attribute)
+        self.events << event
+      end
     end
   end
 
